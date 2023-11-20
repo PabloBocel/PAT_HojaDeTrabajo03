2976d49d5110aeea3c28228e4cee1e6019d7bc8c
Pablo <andresbocel0@gmail.com>
2023-11-20 17:28:15 -0600

    ejercicio2

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -1,6 +1,22 @@
 #include "Ejercicio02.h"
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    return nullptr;
+    std::unordered_map<std::string, std::vector<std::string>> map;
+    for (std::string& str : strings) {
+        std::string key = str;
+        std::sort(key.begin(), key.end());
+        map[key].push_back(str);
+    }
+
+    auto* result = new std::vector<std::vector<std::string>>;
+    for (auto& pair : map) {
+        result->push_back(pair.second);
+    }
+
+    return result;
 }
