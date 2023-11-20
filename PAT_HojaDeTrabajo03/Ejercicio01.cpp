11e682e8b3ff8519efa34b1959ba92967d41322c
Pablo <andresbocel0@gmail.com>
2023-11-20 17:22:33 -0600

    ejercicio1

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -1,6 +1,40 @@
 #include "Ejercicio01.h"
 
+
+int partition(int nums[], int n) {
+    int pivot = nums[n - 1];
+    int i = -1;
+
+    for (int j = 0; j < n; j++)
+    {
+        if (nums[j] < pivot) {
+            i++;
+            int aux = nums[i];
+            nums[i] = nums[j];
+            nums[j] = aux;
+        }
+    }
+
+    i++;
+    int aux = nums[i];
+    nums[i] = nums[n - 1];
+    nums[n - 1] = aux;
+
+    return i;
+}
+
+void quicksort(int nums[], int n) {
+    if (n <= 1)
+        return;
+    int pi = partition(nums, n);
+    quicksort(nums, pi);
+    quicksort(nums + pi + 1, n - pi - 1);
+}
+
+
 int Ejercicio01::findMin(int nums[], int n)
 {
-    return 0;
+    quicksort(nums, n);
+    return nums[0];
 }
+
