d3c6f12edb4b3f3bc58fd3f1287d0a5ba4638b98
Pablo <andresbocel0@gmail.com>
2023-11-20 17:36:44 -0600

    Listo

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -2,5 +2,36 @@
 
 Node<int>* Ejercicio03::copyList(Node<int>* head)
 {
-    return nullptr;
+    if (!head) {
+        return nullptr;
+    }
+
+    Node<int>* current = head;
+    while (current) {
+        Node<int>* newNode = new Node<int>{ current->value, nullptr, nullptr };
+        newNode->next = current->next;
+        current->next = newNode;
+        current = newNode->next;
+    }
+
+    current = head;
+    while (current) {
+        if (current->random) {
+            current->next->random = current->random->next;
+        }
+        current = current->next->next;
+    }
+
+    Node<int>* newHead = head->next;
+    current = head;
+    while (current) {
+        Node<int>* temp = current->next;
+        current->next = temp->next;
+        current = current->next;
+        if (current) {
+            temp->next = current->next;
+        }
+    }
+
+    return newHead;
 }
